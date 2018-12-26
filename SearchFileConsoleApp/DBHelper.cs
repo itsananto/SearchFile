1d9861e0ee6f90cce62e295534331ff5f3681be1
Ananto Ghosh <Ananto Ghosh its.ananto@gmail.com>
2018-12-27 01:37:37 +0600

    Code cleanup

---
+++ SearchFileConsoleApp/DBHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchFileConsoleApp
+{
+    class DBHelper
+    {
+        private SQLiteConnection db;
+
+        public DBHelper()
+        {
+            //TODO: Replace path with fillowing: Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SearchFile", "SearchFile.db3")
+            db = new SQLiteConnection(@"Data Source=Test.db3;Pooling=true;FailIfMissing=false;Version=3");
+            InitializeDatabase();
+        }
+
+        public void InitializeDatabase()
+        {
+            db.Open();
+
+            String tableCommand = "CREATE TABLE IF NOT " +
+                "EXISTS File_Archive (Primary_Key INTEGER PRIMARY KEY, " +
+                "Text_Entry NVARCHAR(2048) NULL)";
+
+            SQLiteCommand createTable = new SQLiteCommand(tableCommand, db);
+
+            createTable.ExecuteReader();
+
+
+            tableCommand = "CREATE TABLE IF NOT " +
+                "EXISTS Search_History (Primary_Key INTEGER PRIMARY KEY, " +
+                "Time INTEGER NOT NULL)";
+
+            createTable = new SQLiteCommand(tableCommand, db);
+
+            createTable.ExecuteReader();
+        }
+
+        public void PopulateFileArchiveTable(StringCollection files)
+        {
+            using (SQLiteConnection db = new SQLiteConnection(@"Data Source=Test.db3;Pooling=true;FailIfMissing=false;Version=3"))
+            {
+                db.Open();
+
+                SQLiteCommand insertCommand = new SQLiteCommand();
+                insertCommand.Connection = db;
+
+                using (var transaction = db.BeginTransaction())
+                {
+
+                    foreach (var file in files)
+                    {
+                        insertCommand.CommandText = "INSERT INTO File_Archive VALUES (NULL, @Entry);";
+                        insertCommand.Parameters.AddWithValue("@Entry", file);
+
+                        insertCommand.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+
+                }
+
+                db.Close();
+            }
+
+        }
+    }
+}
--- SearchFileConsoleApp/Program.cs
+++ SearchFileConsoleApp/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +13,24 @@ namespace SearchFileConsoleApp
     class Program
     {
         static System.Collections.Specialized.StringCollection log = new System.Collections.Specialized.StringCollection();
+        static System.Collections.Specialized.StringCollection fileLog = new System.Collections.Specialized.StringCollection();
+
+        
 
         static void Main(string[] args)
         {
+            InitializeDatabase();
+            //AddData("hh");
+            //Run();
+
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.File("log.txt",
+                rollingInterval: RollingInterval.Day,
+                rollOnFileSizeLimit: true)
+            .CreateLogger();
+
+
             // Start with drives if you have to search the entire computer.
             string[] drives = System.Environment.GetLogicalDrives();
 
@@ -27,9 +46,11 @@ namespace SearchFileConsoleApp
                     continue;
                 }
                 System.IO.DirectoryInfo rootDir = di.RootDirectory;
-                WalkDirectoryTree(rootDir);
+                //TraverseTree(rootDir.Name);
             }
 
+            TraverseTree("D:\\");
+            AddData();
             // Write out all the files that could not be processed.
             Console.WriteLine("Files with restricted access:");
             foreach (string s in log)
@@ -37,55 +58,196 @@ namespace SearchFileConsoleApp
                 Console.WriteLine(s);
             }
             // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key");
-            Console.ReadKey();
+            //Console.WriteLine("Press any key");
+            //Console.ReadKey();
         }
 
-        static void WalkDirectoryTree(System.IO.DirectoryInfo root)
+        public static void InitializeDatabase()
         {
-            System.IO.FileInfo[] files = null;
-            System.IO.DirectoryInfo[] subDirs = null;
+            var fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"SearchFile", "SearchFile.db3");
+            using (SQLiteConnection db =
+                new SQLiteConnection(@"Data Source=Test.db3;Pooling=true;FailIfMissing=false;Version=3"))
+            {
+                db.Open();
 
-            // First, process all the files directly under this folder
-            try
-            {
-                files = root.GetFiles("*.*");
-            }
-            // This is thrown if even one of the files requires permissions greater
-            // than the application provides.
-            catch (UnauthorizedAccessException e)
-            {
-                // This code just writes out the message and continues to recurse.
-                // You may decide to do something different here. For example, you
-                // can try to elevate your privileges and access the file again.
-                log.Add(e.Message);
-            }
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
 
-            catch (System.IO.DirectoryNotFoundException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+                SQLiteCommand createTable = new SQLiteCommand(tableCommand, db);
 
-            if (files != null)
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddData()
+        {
+            using (SQLiteConnection db = new SQLiteConnection(@"Data Source=Test.db3;Pooling=true;FailIfMissing=false;Version=3"))
             {
-                foreach (System.IO.FileInfo fi in files)
+                db.Open();
+
+                SQLiteCommand insertCommand = new SQLiteCommand();
+                insertCommand.Connection = db;
+
+                using (var transaction = db.BeginTransaction())
                 {
-                    // In this example, we only access the existing FileInfo object. If we
-                    // want to open, delete or modify the file, then
-                    // a try-catch block is required here to handle the case
-                    // where the file has been deleted since the call to TraverseTree().
-                    Console.WriteLine(fi.FullName);
+
+                    foreach (var item in fileLog)
+                    {
+
+                        // Use parameterized query to prevent SQL injection attacks
+                        insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
+                        insertCommand.Parameters.AddWithValue("@Entry", item);
+
+                        insertCommand.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+
                 }
 
-                // Now find all the subdirectories under this directory.
-                subDirs = root.GetDirectories();
+                db.Close();
+            }
 
-                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+        }
+
+        public static void TraverseTree(string root)
+        {
+            // Data structure to hold names of subfolders to be
+            // examined for files.
+            Stack<string> dirs = new Stack<string>(20);
+
+            if (!System.IO.Directory.Exists(root))
+            {
+                throw new ArgumentException();
+            }
+            dirs.Push(root);
+
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+                string[] subDirs;
+                try
                 {
-                    // Resursive call for each subdirectory.
-                    WalkDirectoryTree(dirInfo);
+                    subDirs = System.IO.Directory.GetDirectories(currentDir);
+                }
+                // An UnauthorizedAccessException exception will be thrown if we do not have
+                // discovery permission on a folder or file. It may or may not be acceptable 
+                // to ignore the exception and continue enumerating the remaining files and 
+                // folders. It is also possible (but unlikely) that a DirectoryNotFound exception 
+                // will be raised. This will happen if currentDir has been deleted by
+                // another application or thread after our call to Directory.Exists. The 
+                // choice of which exceptions to catch depends entirely on the specific task 
+                // you are intending to perform and also on how much you know with certainty 
+                // about the systems on which this code will run.
+                catch (UnauthorizedAccessException e)
+                {
+                   // Console.WriteLine(e.Message);
+                    continue;
+                }
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                   // Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                string[] files = null;
+                try
+                {
+                    files = System.IO.Directory.GetFiles(currentDir);
+                }
+
+                catch (UnauthorizedAccessException e)
+                {
+
+                   // Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                   // Console.WriteLine(e.Message);
+                    continue;
+                }
+                // Perform the required action on each file here.
+                // Modify this block to perform your required task.
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        // Perform whatever action is required in your scenario.
+                        System.IO.FileInfo fi = new System.IO.FileInfo(file);
+                        //Console.WriteLine("{0}: {1}, {2}", fi.Name, fi.Length, fi.CreationTime);
+                        fileLog.Add(fi.Name);
+                        Log.Information(fi.Name + " " + fi.LastAccessTime);
+                    }
+                    catch (System.IO.FileNotFoundException e)
+                    {
+                        // If file was deleted by a separate application
+                        //  or thread since the call to TraverseTree()
+                        // then just continue.
+                       // Console.WriteLine(e.Message);
+                        continue;
+                    }
+                    catch (Exception e)
+                    {
+                        // If file was deleted by a separate application
+                        //  or thread since the call to TraverseTree()
+                        // then just continue.
+                       // Console.WriteLine(e.Message);
+                        continue;
+                    }
+                }
+
+                // Push the subdirectories onto the stack for traversal.
+                // This could also be done before handing the files.
+                foreach (string str in subDirs)
+                {
+                    dirs.Push(str);
                 }
             }
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public static void Run()
+        {
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = "D:\\";
+            watcher.IncludeSubdirectories = true;
+        
+            /* Watch for changes in LastAccess and LastWrite times, and
+               the renaming of files or directories. */
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            // Only watch text files.
+            watcher.Filter = "*.*";
+
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+
+            // Wait for the user to quit the program.
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
+        }
+
+        // Define the event handlers.
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
         }
     }
 }
